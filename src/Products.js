4428ce4009933ce846c1e10fafb0a15becc1e0bd
Joey <jxzheng16@gmail.com>
2021-04-29 18:03:11 -0400

    initial commit

---
+++ src/Products.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+class Products extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="products">
+        {/* Display products below */}
+      </div>
+    )
+  }
+
+}
+
+
+export default Products;
---
+++ src/Wishlist.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+class Wishlist extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="wishlist">
+        <h1>Wishlist (Items added go down below)</h1>
+      </div>
+    )
+  }
+
+}
+
+
+export default Wishlist;
